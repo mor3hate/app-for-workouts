9e335fc932bf9c1075ba92e36cd2f77da6fad53f
morehate <thelokkedmail3@gmail.com>
2022-08-12 03:55:24 +0300

    Server.js changes

--- back/server.js
+++ back/server.js
@@ -30,10 +30,10 @@ app.use('/api/workouts', workoutRoutes)
 
 if (process.env.NODE_ENV === 'production') {
 	// Step 1:
-	app.use(express.static(path.resolve(__dirname, './client/build')))
+	app.use(express.static(path.resolve(__dirname, './front/build')))
 	// Step 2:
 	app.get('*', function (request, response) {
-		response.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+		response.sendFile(path.resolve(__dirname, './front/build', 'index.html'))
 	})
 }
 
